d4725bb6090a0a8185ec705449c60a418a5766a7
Arthur Sengileyev <arthur.sengileyev@gmail.com>
2023-03-21 14:11:27 +0200

    Revert "Minor fix"
    This reverts commit a5c779f88cd779ee943afb95bef05e10620995ac.

--- pkg/machine/qemu/machine.go
+++ pkg/machine/qemu/machine.go
@@ -1874,5 +1874,8 @@ func retryRecoverableWithDelay(n int, wait time.Duration, factor float64, f func
 		time.Sleep(wait)
 		wait = time.Duration(float64(wait) * factor)
 	}
-	return rec
+	if rec != nil {
+		return rec
+	}
+	return nil
 }
